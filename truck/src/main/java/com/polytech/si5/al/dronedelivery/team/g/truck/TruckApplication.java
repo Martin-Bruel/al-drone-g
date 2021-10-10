3d44fc059bfa9e45b195f14eb6752d94e5534233
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2021-10-10 21:49:56 +0200

    #16 Added data insertion demo

--- truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/TruckApplication.java
+++ truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/TruckApplication.java
@@ -1,9 +1,13 @@
 package com.polytech.si5.al.dronedelivery.team.g.truck;
 
+import com.polytech.si5.al.dronedelivery.team.g.truck.dao.DroneDAO;
+import com.polytech.si5.al.dronedelivery.team.g.truck.entities.Drone;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
@@ -16,4 +20,22 @@ public class TruckApplication {
 		SpringApplication.run(TruckApplication.class, args);
 	}
 
+	@Bean
+	public CommandLineRunner demo(DroneDAO repository) {
+		return (args) -> {
+			// save a few drones
+			repository.save(new Drone("Alpha"));
+			repository.save(new Drone("Tango"));
+			repository.save(new Drone("Charly"));
+
+
+			// fetch all drones
+			log.info("Drones found with findAll():");
+			log.info("-------------------------------");
+			for (Drone drone : repository.findAll()) {
+				log.info(drone.toString());
+			}
+			log.info("");
+		};
+	}
 }
