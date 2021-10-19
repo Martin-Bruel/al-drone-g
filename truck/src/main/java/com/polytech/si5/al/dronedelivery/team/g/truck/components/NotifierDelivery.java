67e64357daf5a3ff2d3c4e479ec1e2589ee1bc05
Thibaut-Esteve <estevet@hotmail.fr>
2021-10-19 23:01:42 +0200

    Revert "#28 test persistance des notifications"
    This reverts commit 0ca5f8fb9eca36bb959ba5461a9a3b2f545b4182.

--- truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/components/NotifierDelivery.java
+++ truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/components/NotifierDelivery.java
@@ -1,7 +1,6 @@
 package com.polytech.si5.al.dronedelivery.team.g.truck.components;
 
 import com.polytech.si5.al.dronedelivery.team.g.truck.entities.Notification;
-import com.polytech.si5.al.dronedelivery.team.g.truck.interfaces.NotificationRegistration;
 import com.polytech.si5.al.dronedelivery.team.g.truck.interfaces.Notifier;
 import com.polytech.si5.al.dronedelivery.team.g.truck.services.WarehouseService;
 import org.slf4j.Logger;
@@ -9,50 +8,18 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import javax.transaction.Transactional;
-import java.util.List;
-
 @Component
-public class NotifierDelivery implements Notifier, NotificationRegistration {
+public class NotifierDelivery implements Notifier {
 
     @Autowired
     WarehouseService warehouseService;
 
-    @PersistenceContext
-    private EntityManager entityManager;
     private static final Logger logger = LoggerFactory.getLogger(NotifierDelivery.class);
 
     @Override
     public void notifyDelivered(long packageId, int deliverySate) {
         logger.info("Notify packageId : " + packageId+", deliverySate : "+deliverySate);
         Notification notification = new Notification(packageId, deliverySate);
-        testPersistNotification(notification);
         warehouseService.sendNotification(notification);
-    }
-
-    private void testPersistNotification(Notification notification){
-        System.out.println("Notification to persist = "+notification);
-        registerNotification(notification);
-        System.out.println("Persisted");
-        System.out.println("Notifications in database :");
-        List<Notification> Notifications = getAllNotification();
-        for(Notification n : Notifications){
-            System.out.println(n);
-        }
-        System.out.println("End of the test for the notifications");
-    }
-
-    @Override
-    @Transactional
-    public void registerNotification(Notification n) {
-        entityManager.persist(n);
-    }
-
-    @Transactional
-    public List<Notification> getAllNotification(){
-        return (List<Notification>) entityManager.createQuery(
-                "SELECT e FROM Notification e", Notification.class).getResultList();
     }
 }
--- truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/entities/Notification.java
+++ truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/entities/Notification.java
@@ -2,23 +2,8 @@ package com.polytech.si5.al.dronedelivery.team.g.truck.entities;
 
 
 import com.polytech.si5.al.dronedelivery.team.g.truck.constants.DeliveryStatusCode;
-import lombok.AccessLevel;
-import lombok.Getter;
-import lombok.Setter;
-import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
-import java.util.Objects;
-
-@Entity
-@Getter
-@Setter
 public class Notification {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    @Column(name = "id", nullable = false)
-    @Setter(AccessLevel.NONE)
-    private Long id;
 
     private String description;
 
@@ -30,18 +15,5 @@ public class Notification {
         }
     }
 
-    public Notification() {
-        this.description = "An empty notification";
-    }
-
-    @Override
-    public String toString() {
-        return String.format(
-                "Notification[id = %d, description= %s]", id, description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id,description);
-    }
+    public String getDescription() { return description; }
 }
