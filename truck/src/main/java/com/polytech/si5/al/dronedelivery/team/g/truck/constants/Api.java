0ef29dea25a4e3f291ea8d7700541d332d687298
NathanMeulle <54913885+NathanMeulle@users.noreply.github.com>
2021-10-22 10:22:01 +0200

    Fix - Dsl Martin

--- truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/constants/Api.java
+++ truck/src/main/java/com/polytech/si5/al/dronedelivery/team/g/truck/constants/Api.java
@@ -2,7 +2,7 @@ package com.polytech.si5.al.dronedelivery.team.g.truck.constants;
 
 public class Api {
     public static final String DEFAULT_ENV = "dev";
-    public static final String ENV = System.getenv("APP_ENV") ? System.getenv("APP_ENV") : DEFAULT_ENV;
+    public static final String ENV = System.getenv("APP_ENV") != null ? System.getenv("APP_ENV") : DEFAULT_ENV;
     public static final int RESTTEMPLATE_TIMEOUT = 5000; //ms
     public static final int DRONE_TIMEOUT = 3000;
     public static final int DRONE_RETRY_CONNECTION = 5;
