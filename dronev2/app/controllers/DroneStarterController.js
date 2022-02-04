28f8eb2b0659fdc0d7b307b3df1183f3b5e25865
DavidLebrisse <david.lebrisse@etu.univ-cotedazur.fr>
2022-02-04 11:03:19 +0100

    Cleaned up logs

--- dronev2/app/controllers/DroneStarterController.js
+++ dronev2/app/controllers/DroneStarterController.js
@@ -3,7 +3,6 @@ const { FlightPlan } = require('../model/FlightPlan');
 const { Fleet } = require('../model/Fleet');
 
 exports.deliveryStart = async function(req, res){
-    console.log(req.body)
     let flightPlan = new FlightPlan(req.body.flightPlan.steps);
     let fleet = new Fleet(req.body.fleet.drones);
     let leaderDroneId = req.body.fleet.leaderDroneId;
